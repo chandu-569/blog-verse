f00bbd8bdaac0aea808d8827c9420ca1bf308039
chandu-569 <chanduchintakayala2@gmail.com>
2025-09-06 15:54:36 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,39 @@
+function hello()
+{
+    console.log("hello bro");
+}
+hello();
+
+
+function sum(x,y)
+{
+    console.log(x+y);
+}
+sum(10,20);
+
+const pi=3.14
+console.log(pi)
+
+
+const subtract=function sub(x,y)
+{
+    console.log(x-y);
+    return x-y;
+}
+console.log(subtract(5,2))
+
+const square=(x)=>x*x
+console.log(square(5))
+
+
+const subtracts=(x,y)=>
+{
+    console.log(x,y);
+    return x-y;
+}
+
+
+console.log(20,15)
+console.log(subtracts(20,15))
+
+
