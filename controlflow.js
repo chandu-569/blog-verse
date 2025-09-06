a4adcc733960b6743d27806fdb8633f8e4bfc650
chandu-569 <chanduchintakayala2@gmail.com>
2025-09-06 15:03:51 +0530

    controlflow

---
+++ controlflow.js
@@ -0,0 +1,17 @@
+let marks=85;
+if(marks>=90)
+{
+    console.log("A+");
+}
+else if(marks>=80)
+{
+    console.log("A");
+}
+else if(marks>=70)
+{
+    console.log("b");
+}
+else 
+{
+    console.log("c");
+}
