664c35261a8d3a98522888a97e719bcdd8499753
chandu-569 <chanduchintakayala2@gmail.com>
2025-09-06 12:34:57 +0530

    new

--- practice.js
+++ practice.js
@@ -1,1 +1,26 @@
 console.log("hello")
+let name="chandu";
+console.log(name)
+
+let num=69;
+console.log(num)
+
+let islogged="false";
+console.log(islogged);
+
+
+let ch;
+let x;
+console.log(x);
+console.log(typeof ch);
+
+let arr=[1,3,2,3];
+console.log([0]);
+console.log(typeof arr);
+
+let person={
+    name:"chandu",
+    age:20
+}
+console.log(person.name)
+console.log(person.age)
