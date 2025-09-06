bd15493fe87febf412fc4e9e0942dd403bc3ca23
chandu-569 <chanduchintakayala2@gmail.com>
2025-09-06 16:47:13 +0530

    main

---
+++ Main.js
@@ -0,0 +1,4 @@
+import {add,sub,multiply} from "./Math.js"
+console.log(add(2,2))
+console.log(sub(3,4))
+console.log(multiply(5,5))
---
+++ Math.js
@@ -0,0 +1,48 @@
+/*function sum(x,y)
+{
+    console.log(x+y);
+}
+sum(10,20);
+
+const pi=3.14
+console.log(pi)
+
+
+const subtract=function sub(x,y)
+{
+    console.log(x-y);
+    return x-y;
+}
+console.log(subtract(5,2))
+
+const square=(x)=>x*x
+console.log(square(5))
+
+
+const subtracts=(x,y)=>
+{
+    console.log(x,y);
+    return x-y;
+}
+
+
+console.log(20,15)
+console.log(subtracts(20,15))
+
+*/
+
+export const add=function(x,y)
+{
+    return x+y;
+}
+
+export const sub=function(x,y)
+{
+    return x-y;
+}
+
+
+export const multiply=function(x,y)
+{
+    return x*y;
+}
